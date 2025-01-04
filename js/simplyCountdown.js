86eb82ac2fce6dcf75cba5417c62aeeac20d3787
avelacam <avelacam@emeal.nttdata.com>
2025-01-04 13:37:35 -0700

    changes

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -122,10 +122,10 @@
                 minutes: 0,
                 seconds: 0,
                 words: {
-                    days: 'day',
-                    hours: 'hour',
-                    minutes: 'minute',
-                    seconds: 'second',
+                    days: 'Dia',
+                    hours: 'Hora',
+                    minutes: 'Minuto',
+                    seconds: 'Segundo',
                     pluralLetter: 's'
                 },
                 plural: true,
